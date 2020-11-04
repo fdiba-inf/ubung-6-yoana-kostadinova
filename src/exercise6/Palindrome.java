afe3118aed3761a2f6894f3afeb8a32b97129014
yoana-kostadinova <ani.kostadinova2001@gmail.com>
2020-11-04 19:06:30 +0200

    test1

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -4,6 +4,29 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+
+        System.out.println ("Enter a number: ");
+
+        int number = input.nextInt ();
+        assert number > 0 : "*** Number is not bigger than 0! ***";
+
+        int reversedNumber = 0;
+        int number2 = number;
+
+            while (number2 > 0) {
+                reversedNumber = (reversedNumber * 10) + (number2 % 10);
+                number2 = number2 / 10;
+            }
+
+            if (reversedNumber == number)
+        {
+            System.out.println ("Number is palindrome");
+        }
+
+        else {
+            System.out.println ("Number is NOT palindrome");
+        }
     }
 }
